ecc8b898100ad5d968fe6aabeebebd434b48c7da
Dmytriy Sharov <dima.sharov99.ds@gmail.com>
2024-07-09 12:17:08 +0200

    ReadMe e url update

--- src/app/service/api.export.ts
+++ src/app/service/api.export.ts
@@ -7,15 +7,12 @@ export const token: string = localStorage.getItem('token')!;
 // });
 // export const httpOption = { headers: headers };
 
-export const urlUser: string = 'http://localhost:3000/api/user';
-export const urlPost: string = 'http://localhost:3000/api/post';
-export const urlAuth: string = 'http://localhost:3000/api';
 // export const urlPost: string = 'https://epichub.onrender.com/api/post';
 // export const urlUser: string = 'https://epichub.onrender.com/api/user';
 // export const urlAuth: string = 'https://epichub.onrender.com/api';
-// export const urlPost: string = 'https://harmony-4uc7.onrender.com/api/post';
-// export const urlUser: string = 'https://harmony-4uc7.onrender.com/api/user';
-// export const urlAuth: string = 'https://harmony-4uc7.onrender.com/api';
+export const urlPost: string = 'https://harmony-4uc7.onrender.com/api/post';
+export const urlUser: string = 'https://harmony-4uc7.onrender.com/api/user';
+export const urlAuth: string = 'https://harmony-4uc7.onrender.com/api';
 
 const headers = new HttpHeaders({
   Authorization: token,
