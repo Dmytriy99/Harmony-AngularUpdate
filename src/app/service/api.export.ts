0cde40373dfab6589ad465c31ee0833058611ecb
Dmytriy Sharov <dima.sharov99.ds@gmail.com>
2024-07-15 23:55:15 +0200

    fix loading e deploy server

--- src/app/service/api.export.ts
+++ src/app/service/api.export.ts
@@ -2,12 +2,15 @@ import { HttpHeaders } from '@angular/common/http';
 
 export const token: string = localStorage.getItem('token')!;
 
-export const urlUser: string = 'http://localhost:3000/api/user';
-export const urlPost: string = 'http://localhost:3000/api/post';
-export const urlAuth: string = 'http://localhost:3000/api';
+// export const urlUser: string = 'http://localhost:3000/api/user';
+// export const urlPost: string = 'http://localhost:3000/api/post';
+// export const urlAuth: string = 'http://localhost:3000/api';
 // export const urlPost: string = 'https://harmony-4uc7.onrender.com/api/post';
 // export const urlUser: string = 'https://harmony-4uc7.onrender.com/api/user';
 // export const urlAuth: string = 'https://harmony-4uc7.onrender.com/api';
+export const urlPost: string = 'https://harmony-1.onrender.com/api/post';
+export const urlUser: string = 'https://harmony-1.onrender.com/api/user';
+export const urlAuth: string = 'https://harmony-1.onrender.com/api';
 
 const headers = new HttpHeaders({
   Authorization: token,
